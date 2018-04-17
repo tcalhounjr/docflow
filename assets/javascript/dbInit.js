273e1dbbdb348438d18a9a607f07d5e0461b32a4
tcalhounjr <thomas@nafasi.co>
2018-04-17 12:31:45 -0400

    Finalized the landing page look and feel

---
+++ assets/javascript/dbInit.js
@@ -0,0 +1,22 @@
+var db = {
+    initializeDatabase: function() {
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyAPGoG4Ely2k_ql5CyzUffcHNE2eOhY8_o",
+            authDomain: "sharktank-docflow.firebaseapp.com",
+            databaseURL: "https://sharktank-docflow.firebaseio.com",
+            projectId: "sharktank-docflow",
+            storageBucket: "sharktank-docflow.appspot.com",
+            messagingSenderId: "133287952779"
+        };
+        firebase.initializeApp(config);
+        var database = firebase.database();
+        return(database);
+    }
+}
+
+//Make this db initialization accessible to other files within the app
+
+module.exports = {
+    db: db
+};
